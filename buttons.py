607e919de83fca3961512053a46db67af08642b4
dnlpntl <rndm246@gmail.com>
2020-12-01 16:15:25 +0100

    0.2

---
+++ buttons.py
@@ -0,0 +1,31 @@
+import pygame
+from konstanten import *
+from Scenes import GameScene
+from SceneBase import SceneBase
+
+
+def text_objects(text, font):
+    text_surface = font.render(text, True, (255, 255, 255))
+    return text_surface
+
+
+def button(text, x, y, width_btn, height_btn, active_color, inactive_color, screen, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    # start game Button
+    if x < mouse[0] < x + width_btn and y < mouse[1] < y + height_btn:
+        pygame.draw.rect(screen, active_color,
+                         (x, y, width_btn, height_btn))
+        if click[0] == 1 and action != None:
+            action()
+
+    else:
+        pygame.draw.rect(screen, inactive_color,
+                         (x, y, width_btn, height_btn))
+
+    text_font_small = pygame.font.Font(None, 32)
+    text_surface = text_objects(text, text_font_small)
+    text_rect = text_surface.get_rect(
+        center=((x + (width_btn / 2)), (y + (height_btn / 2))))
+    screen.blit(text_surface, text_rect)
---
+++ textboxes.py
@@ -0,0 +1,32 @@
+from konstanten import *
+import pygame
+
+
+class TextBox:
+
+    FONT = pygame.font.Font(None, 32)
+
+    def __init__(self, rect, text=''):
+        self.rect = pygame.Rect(rect)
+        self.color = pygame.Color(20, 200, 20)
+        self.text = text
+        self.txt_surface = pygame.font.Font(
+            None, 32).render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = pygame.Color(200, 200, 20)
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
