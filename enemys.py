bffee437c3d10940ef849f4ca1723fc7dc49ca72
Jordan Rahn <jordan.rahn@freenet.de>
2020-12-13 19:27:16 +0100

    Enemys moven nach unten

--- enemys.py
+++ enemys.py
@@ -3,59 +3,59 @@ from assetloader import Assetloader
 import pygame
 import random
 
+
 class IEnemyShip:
 
-    # Interface Init
+    
     def __init__(self, game):
-        # X Koordinate des Ship
+        # X Koordinate des Enemy
         self.Ship_X = 0
-        # Y Koordinate des Ship
+        # Y Koordinate des Enemy
         self.Ship_Y = 0
-        # Das Ship als Bild
+        # Enemy als Bild
         self.Ship = None
-        # Das Ship als Rect
+        # Enemy als Rect
         self.ShipRect = None
         # Zugriffsvariable 
         self.game = game
         # Zählervariable
         self.move_counter = 0
-        # Ship bewegt sich nach rechts wegen positivem Wert
-        self.move_direction = 1
+        # Enemybewegung auf x-Achse bei positivem Wert nach rechts, bei negativem Wert nach links
+        self.move_direction_x = 1
+        # Enemybewegung auf y-Achse 
+        self.move_direction_y = 25
         
 
-    # Methode zum Zeichnen der Objekte
+    # Methode zum Zeichnen der Enemys
     def draw(self):
         self.moveShipRect()
 
-    # Methode zum Updaten der Objekte
+    # Methode zum Updaten der Enemys
     def update(self):
         pass
 
-    # Methode zum zufälligen Startpunkt (X) für ein Objekt
+    # Enemys als Rect
     def getShipRect(self):
         self.ShipRect = pygame.Rect(self.Ship_X, self.Ship_Y, 30, 30)
         return self.ShipRect
 
-    # Methode zum Bewegen eines Objektes
+    # Methode zum Bewegen der Enemys
     def moveShipRect(self):
-        # Ship um sich selbst drehen lassen
+        # Asset um 90 Grad drehen
         self.Ship = pygame.transform.rotate(self.Ship, -90)
-        # Ship runterskalieren auf 100x100
+        # Asset runterskalieren auf 100x100 Pixel
         self.Ship = pygame.transform.scale(self.Ship, (100, 100))
         
-        self.Ship_X  += self.move_direction
+        # Ship von links nach rechts bewegen & nach unten 
+        self.Ship_X  += self.move_direction_x
         self.move_counter += 1
-        if abs(self.move_counter) > 200:
-            self.move_direction *= -1
-            self.move_counter = self.move_direction * self.move_direction
-        #print(self.Ship_X)
-        #print(self.move_counter)
-        # Überprüfung ob das Item X innerhalb des Bildschirms ist
-        #if (self.Ship_X <= (self.game.width + (self.Ship_Row) - 100)):
-        #    # Item mit den zufälligen Item Speed von oben nach unten bewegen lassen
-        #    self.Ship_X  = self.Ship_X + self.Ship_Speed
-
-        # Item zeichnen
+        if abs(self.move_counter) > 500:
+            self.move_direction_x *= -1
+            self.Ship_Y += self.move_direction_y
+            self.move_counter = 0
+            #self.move_counter = self.move_direction_x * self.move_direction_x
+        
+        # Enemys auf Bildschirm zeichnen
         self.game.screen.blit(self.Ship, self.ShipRect)
 
         
@@ -63,28 +63,19 @@ class IEnemyShip:
 
 class Enemy_One(IEnemyShip):
     def __init__(self, game, row, cell):
-        # X Koordinate des Ship
         self.Ship_X = (40 * row)
-        # Y Koordinate des Ship
         self.Ship_Y = (70 * cell)
-
         self.Ship_Row = row
-
         self.Ship_Cell = cell
-        # Das Ship als Bild
         self.Ship = None
-        # Das Ship als Rect
-        self.ShipRect = None
-        # Zugriffsvariable 
+        self.ShipRect = None 
         self.game = game
         self.move_counter = 0
-        self.move_direction = 1
+        self.move_direction_x = 1
+        self.move_direction_y = 25
         
     # Override der Update Methode
     def update(self):
         # Ship Rect erzeugen
         self.Ship = Assetloader.getAsset(AssetType.Graphics, "Ship2.png")
         self.ShipRect = self.getShipRect()
-
- 
- 
