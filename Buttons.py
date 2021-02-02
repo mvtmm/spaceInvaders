dd3bcae35a94ed72af884fa22c23299815564947
dnlpntl <rndm246@gmail.com>
2021-02-02 16:34:23 +0100

    Kommentare wurden hinzugefeatured

--- Buttons.py
+++ Buttons.py
@@ -1,6 +1,8 @@
 import pygame
 from Konstanten import *
 
+# Textobjekt erstellen mit Farbe und Schriftart
+
 
 def text_objects(text, font):
     text_surface = font.render(text, True, (255, 255, 255))
@@ -11,7 +13,7 @@ def button(text, x, y, width_btn, height_btn, active_color, inactive_color, scre
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    # start game Button
+    # Hovereffekt Farbwechsel
     if x < mouse[0] < x + width_btn and y < mouse[1] < y + height_btn:
         pygame.draw.rect(screen, active_color.value,
                          (x, y, width_btn, height_btn))
@@ -21,9 +23,11 @@ def button(text, x, y, width_btn, height_btn, active_color, inactive_color, scre
     else:
         pygame.draw.rect(screen, inactive_color.value,
                          (x, y, width_btn, height_btn))
-
+    # Schriftgröße festlegen
     text_font_small = pygame.font.Font(None, 32)
     text_surface = text_objects(text, text_font_small)
+    # Schrift zentrieren im Rect
     text_rect = text_surface.get_rect(
         center=((x + (width_btn / 2)), (y + (height_btn / 2))))
+    # Button erzeugen
     screen.blit(text_surface, text_rect)
