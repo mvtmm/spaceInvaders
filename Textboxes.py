addf481d7a483450c52519d25aac500b5b6383d1
dnlpntl <rndm246@gmail.com>
2021-02-02 16:45:25 +0100

    Arbeit Arbeit !

--- Textboxes.py
+++ Textboxes.py
@@ -3,22 +3,25 @@ import pygame
 
 
 class TextBox:
-
+    # Erzeugen einer Box mit Farbe & Text
     def __init__(self, rect, text=''):
         self.rect = pygame.Rect(rect)
         self.color = pygame.Color(20, 200, 20)
         self.text = text
-        self.txt_surface = pygame.font.Font(None, 32).render(text, True, self.color)
+        self.txt_surface = pygame.font.Font(
+            None, 32).render(text, True, self.color)
         self.active = False
+    # zeichnet textbox
 
     def draw(self, screen):
-        # Blit the text.
+        # Zeichnen Textbox mit Rand in gleicher Farbe
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
-        # Blit the rect.
         pygame.draw.rect(screen, self.color, self.rect, 2)
+    # Update des Textes in der Box
 
     def update(self, screen, text, score):
-
+        # Übergibt den neuen Text
         self.txt_surface = pygame.font.Font(
             None, 32).render(text+" "+score, True, self.color)
+        # erzeugt neuen Text
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
